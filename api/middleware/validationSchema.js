d3124db979a764e212bc61267b1651f5a1f85b66
g3or3 <>
2021-05-06 18:50:13 -0400

    dan

--- api/middleware/validationSchema.js
+++ api/middleware/validationSchema.js
@@ -1,26 +1,15 @@
 const Joi = require("joi");
 
 const userSchema = Joi.object({
-	name: Joi.string()
-		.trim()
-		.min(3)
-		.max(30)
-		.required()
-		.messages({
-			"any.required": `"missing required name`,
-		}),
+	name: Joi.string().trim().min(3).max(30).required().messages({
+		"any.required": `"missing required name`,
+	}),
 });
 
 const postSchema = Joi.object({
-	text: Joi.string()
-		.trim()
-		.alphanum()
-		.min(3)
-		.max(100)
-		.required()
-		.messages({
-			"any.required": `"missing required text`,
-		}),
+	text: Joi.string().trim().min(3).max(100).required().messages({
+		"any.required": `"missing required text`,
+	}),
 });
 
 module.exports = {
