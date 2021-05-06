c041b5c4fd5446deecaedf9035856e0c48858b57
g3or3 <>
2021-05-06 18:05:56 -0400

    because francis said so

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const app = require("./api/app");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
+process.env.API_KEY || "gaga";
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
